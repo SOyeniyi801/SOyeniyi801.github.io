f491537c1bb925bd8fa2b4387910c3bcffad05f1
SOyeniyi801 <47873502+SOyeniyi801@users.noreply.github.com>
2019-05-22 07:12:13 -0400

    Cleaned up code

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ $(function(){
 
   let sum = 0
   let questions = [20.76, 45.29, 10.76, 4.50, 7.90]
-  // let randomIndex = generateRandomNumber()
   let questionIndex = generateRandomNumber()
   let targetNumber
 
@@ -21,10 +20,7 @@ $(function(){
      console.log(targetNumber);
     // display number
     $("#question").text(targetNumber.toFixed(2))
-
   }
-
-
 
   function getValue(){
     let userChoice = $(this).attr('id');
@@ -42,7 +38,7 @@ $(function(){
   function compareAnswer(){
     console.log("Am I right");
     if(targetNumber === sum){
-      console.log("Shmoney"); //logic to play soundbit will go here//
+      console.log("Shmoney");
       shake();
       questionIndex = generateRandomNumber();
       // reset sum to zero
@@ -52,15 +48,7 @@ $(function(){
       // increment questionIndex by 1
       // questionIndex = questionIndex + 1
       prepareQuestion(questionIndex)
-
-      // reassiging targetNumber to next number in questions array
-      //targetNumber = questions[questionIndex]
-
-      // display new target number to the page
-      //$("#question").text(targetNumber.toFixed(2))
-      // randomQuestion()
-
-
+      
     }else {
       console.log("Try Again");
       tryAgain();
@@ -70,12 +58,6 @@ $(function(){
   function roundAmount (amount) {
     return (Math.round(amount * 100) / 100)
   }
-
-  // function randomQuestion(){
-  //   for ( let randomIndex = 0; randomIndex < questions.length; randomIndex++) {
-  //   console.log(questions[randomIndex]);
-  //   }
-  // }
 
   $("#reset").click(reset);
 
@@ -102,9 +84,5 @@ $(function(){
   function tryAgain() {
   alert("Oops! Wrong answer. Please try again");
   }
-
-  // function shuffle(questions) {
-  //  return questions.sort(() => Math.random() - 0.5);
-  // }
 
  });
